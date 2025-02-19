47c45f462b57d6740727ab925239567f72f42d30
Vladislav Vasilev <vladen21@yandex.ru>
2025-02-19 21:23:24 +0300

    serytwert

--- training_scrapy/spiders/author.py
+++ training_scrapy/spiders/author.py
@@ -7,4 +7,16 @@ class AuthorSpider(scrapy.Spider):
     start_urls = ["https://quotes.toscrape.com"]
 
     def parse(self, response):
-        pass
+        all_authors = response.css('a[href^="/author/"]')
+        for author_link in all_authors:
+            yield response.follow(author_link, callback=self.parse_author)
+        next_page = response.css('li.next a:attr(href)').get()
+        if next_page is not None:
+            yield response.follow(next_page, callback=self.parse)
+
+    def parse_author(self, response):
+        yield {
+            'name': response.css('h3.author-title::text').get().strip(),
+            'born_date': response.css('span.author-born-date::text').get(),
+            'born_location': response.css('span.author-born-location::text').get()
+        }
