ff73d19aa38482ce1d020c05a032da79cc48ea9e
Vladislav Vasilev <vladen21@yandex.ru>
2025-02-21 16:39:22 +0300

    drgytse

--- training_scrapy/pipelines.py
+++ training_scrapy/pipelines.py
@@ -1,13 +1,37 @@
-# Define your item pipelines here
-#
-# Don't forget to add your pipeline to the ITEM_PIPELINES setting
-# See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
+from sqlalchemy import create_engine, Column, Integer, String, Text
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import Session
 
 
-# useful for handling different item types with a single interface
-from itemadapter import ItemAdapter
+Base = declarative_base()
 
+class Quote(Base):
+    __tablename__ = 'quote'
+    id = Column(Integer, primary_key=True)
+    text = Column(Text())
+    author = Column(String(200))
+    tags = Column(String(400))
 
 class TrainingScrapyPipeline:
     def process_item(self, item, spider):
         return item
+
+class QuotesToDBPipeline:
+    def open_spider(self, spider):
+        engine = create_engine('sqlite:///sqlite.db', echo=True)
+        Base.metadata.create_all(engine)
+        self.session = Session(engine)
+
+    def process_item(self, item, spider):
+        quote = Quote(
+            text=item['text'],
+            author=item['author'],
+            tags=', '.join(item['tags'])
+        )
+        # print('========================================', quote)
+        self.session.add(quote)
+        self.session.commit()
+        return item
+
+    def close_spider(self, spider):
+        self.session.close()
